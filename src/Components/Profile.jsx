1c2426896fdaaa1479529f4a6d278ba93afb11c5
Oghenetega Esedere <arcteggzz@gmail.com>
2022-10-29 22:22:36 +0000

    finished mobile responsiveness

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,9 +1,17 @@
 import avatar from "../images/avatar.jpg"
+import AvatarShareIcon from "../Components/AvatarShareIcon"
+import AvatarShareIconMobile from "../Components/AvatarShareIconMobile"
 import './Profile.css';
 
 function Profile() {
   return (
     <div className="profile">
+        <div className="avatar-profile">
+          <AvatarShareIcon />
+        </div>
+        <div className="avatar-profile-mobile">
+          <AvatarShareIconMobile />
+        </div>
         <img src={avatar} alt="" className="avatar" id="profile__img"/>
         <h1 className="name">Oghenetega Esedere</h1>
         <code id="twitter">Acteggzz</code>
