12463b6249a4056cb941551f5dbc5a68e8963c10
Oghenetega Esedere <arcteggzz@gmail.com>
2022-11-03 15:31:56 +0000

    feat: added routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import './App.css';
 import Profile from "./Components/Profile"
 import Links from "./Components/Links"
 import Footer from "./Components/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Contact from "./Contact"
 
 function App() {
   return (
     <div className="app">
-      <Profile />
-      <Links />
-      <Footer />
+      <BrowserRouter >
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Profile />
+                <Links />
+                <Footer />
+              </div>
+            }
+          />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import Contact from './Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
