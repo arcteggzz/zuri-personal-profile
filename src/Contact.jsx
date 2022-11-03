811e185bbc7c32d9acf6a79592a49ca9a84b8709
Oghenetega Esedere <arcteggzz@gmail.com>
2022-11-03 13:36:20 +0000

    style: finished styling of new elemnts

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,28 +3,52 @@ import Footer from './Components/Footer'
 
 function Contact() {
   return (
-    
-    <form className="Contact" action=".">
-        <div className='form-header'>
-            <h1>Contact Me</h1>
-            <p>Hi there, contact me to ask me about anything you have in mind.</p>
+    <div>
+        <div className="container">
+            <form action=".">
+                <div className='form-header'>
+                    <h1>Contact Me</h1>
+                    <p>Hi there, contact me to ask me about anything you have in mind.</p>
+                </div>
+
+                {/* names */}
+                <div className='names-inputs'>
+                    <div className='text-name-field'>
+                        <label for="first_name">First name</label>
+                        <input id="first_name" type="text" placeholder="Enter your first name" required pattern="^[a-zA-Z]{3,8}\s[a-zA-Z]{3,8}" />
+                    </div>
+                    <div className='text-name-field'>
+                        <label for="last_name">Last name</label>
+                        <input id="first_name" type="text" placeholder="Enter your last name" required pattern="^[a-zA-Z]{3,8}\s[a-zA-Z]{3,8}" />
+                    </div>
+                </div>
+
+                {/* email */}
+                <div className='text-email-field'>
+                    <label for="last_name">Email</label>
+                    <input id="first_name" type="email" placeholder="yourname@email.com" required />
+                </div>
+
+                {/* message */}
+                <div className='text-email-field'>
+                    <label for="last_name">Message</label>
+                    <textarea id="first_name" placeholder="Send me a message and I'll reply you as soon as possible..." required />
+                </div>
+
+                {/* checkbox */}
+                <div class="checkbox">
+                    <input type="checkbox" id="" class="checkbox-box" />
+                    <label class="md:text-[14px] font-[500] text-[#344054] md:leading-[20px]">You agree to providing your data to arcteggzz who may contact you.</label>
+                </div>
+
+                {/* button */}
+                <button className="" type="submit">
+                    Send message
+                </button>
+            </form>
         </div>
-        <div className='names-inputs'>
-            <div className='text-name-field'>
-                <label for="first_name">First name</label>
-                <input id="first_name" type="text" placeholder="Enter your first name" required pattern="^[a-zA-Z]{3,8}\s[a-zA-Z]{3,8}" />
-            </div>
-            <div className='text-name-field'>
-                <label for="last_name">Last name</label>
-                <input id="first_name" type="text" placeholder="Enter your last name" required pattern="^[a-zA-Z]{3,8}\s[a-zA-Z]{3,8}" />
-            </div>
-        </div>
-        <div className='text-email-field'>
-            <label for="last_name">Email</label>
-            <input id="first_name" type="email" placeholder="yourname@email.com" required />
-        </div>
-    </form>
-    
+        <Footer />
+    </div>
   );
 }
 
